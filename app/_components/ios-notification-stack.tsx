6ff244ee6c2cd754c1d751b8beae2e25af14174b
ShrideepP <shrideeppakhare2005@gmail.com>
2025-10-30 19:48:11 +0530

    chore: final tweaks to perfect iOS notification stack animation

--- app/_components/ios-notification-stack.tsx
+++ app/_components/ios-notification-stack.tsx
@@ -38,11 +38,13 @@ const containerVariants: Variants = {
     opacity: 0,
     y: "100%",
     scale: 0,
+    transformOrigin: "center bottom",
   },
   visible: {
     opacity: 1,
     y: 0,
     scale: 1,
+    transformOrigin: "center bottom",
   },
 };
 
@@ -84,100 +86,102 @@ const IOSNotificationStack = () => {
 
   return (
     <div className="flex h-screen w-screen justify-center bg-neutral-50 pt-48">
-      <AnimatePresence>
-        {clearStep <= 1 ? (
-          <motion.div
-            key="container"
-            animate="visible"
-            exit="hidden"
-            variants={containerVariants}
-            className="h-fit w-sm"
-          >
-            {!showLess ? (
-              <motion.header
-                key="header"
-                animate="visible"
-                exit="hidden"
-                variants={headerVars}
-                className="mb-3 flex max-h-96 items-center justify-between overflow-hidden ps-2"
-              >
-                <h1 className="text-lg font-medium text-neutral-950">
-                  Notifications
-                </h1>
-
-                <div className="flex items-center gap-2">
-                  <button
-                    type="button"
-                    onClick={handleShowLess}
-                    className="inline-flex cursor-pointer items-center gap-1 rounded-full bg-neutral-300 p-2 text-neutral-600"
+      <div className={`h-fit w-fit ${showLess ? "" : "overflow-hidden"}`}>
+        <AnimatePresence>
+          {clearStep <= 1 ? (
+            <motion.div
+              animate="visible"
+              exit="hidden"
+              variants={containerVariants}
+              className="h-fit w-sm"
+            >
+              <AnimatePresence>
+                {!showLess ? (
+                  <motion.header
+                    animate="visible"
+                    exit="hidden"
+                    variants={headerVars}
+                    className="mb-3 flex max-h-96 items-center justify-between overflow-hidden ps-2"
                   >
-                    <IconChevronDown className="size-4" />
-                    {!clearStep ? (
-                      <span className="text-xs font-medium">Show Less</span>
-                    ) : null}
-                  </button>
+                    <h1 className="text-lg font-medium text-neutral-950">
+                      Notifications
+                    </h1>
 
-                  <button
-                    type="button"
-                    onClick={() =>
-                      setClearStep((prevClearStep) => prevClearStep + 1)
-                    }
-                    className="inline-flex cursor-pointer items-center gap-1 rounded-full bg-neutral-300 p-2 text-neutral-600"
+                    <div className="flex items-center gap-2">
+                      <button
+                        type="button"
+                        onClick={handleShowLess}
+                        className={`inline-flex ${!clearStep ? "w-28" : "w-8"} cursor-pointer items-center justify-center gap-1 rounded-full bg-neutral-300 p-2 text-neutral-600 transition-all duration-200`}
+                      >
+                        <IconChevronDown className="size-4" />
+                        {!clearStep ? (
+                          <span className="text-xs font-medium">Show Less</span>
+                        ) : null}
+                      </button>
+
+                      <button
+                        type="button"
+                        onClick={() =>
+                          setClearStep((prevClearStep) => prevClearStep + 1)
+                        }
+                        className={`inline-flex ${clearStep ? "w-28" : "w-8"} cursor-pointer items-center justify-center gap-1 rounded-full bg-neutral-300 p-2 text-neutral-600 transition-all duration-200`}
+                      >
+                        {!clearStep ? <IconX className="size-4" /> : null}
+                        {clearStep ? (
+                          <span className="text-xs font-medium">Clear</span>
+                        ) : null}
+                      </button>
+                    </div>
+                  </motion.header>
+                ) : null}
+              </AnimatePresence>
+
+              <div className="relative flex flex-col gap-3">
+                {notifications.map((notification, index) => (
+                  <motion.div
+                    key={notification.id}
+                    custom={index}
+                    layout
+                    animate={showLess ? "collapse" : "expand"}
+                    variants={notifVars}
+                    style={{
+                      position: showLess ? "absolute" : "static",
+                      zIndex: 50 - index,
+                    }}
+                    onClick={() => (showLess ? setShowLess(!showLess) : null)}
+                    className={`flex w-full rounded-2xl bg-neutral-300 p-3 ${showLess ? "cursor-pointer" : ""}`}
                   >
-                    {!clearStep ? <IconX className="size-4" /> : null}
-                    {clearStep ? (
-                      <span className="text-xs font-medium">Clear</span>
-                    ) : null}
-                  </button>
-                </div>
-              </motion.header>
-            ) : null}
+                    <div className="flex min-w-0 grow items-center gap-2">
+                      <div className="relative h-10 w-10 shrink-0">
+                        <Image
+                          src={notification.icon}
+                          alt="App icon"
+                          fill
+                          className="absolute inset-0 object-cover"
+                        />
+                      </div>
 
-            <div className="relative flex flex-col gap-3">
-              {notifications.map((notification, index) => (
-                <motion.div
-                  key={notification.id}
-                  custom={index}
-                  layout
-                  animate={showLess ? "collapse" : "expand"}
-                  variants={notifVars}
-                  style={{
-                    position: showLess ? "absolute" : "static",
-                    zIndex: 50 - index,
-                  }}
-                  onClick={() => (showLess ? setShowLess(!showLess) : null)}
-                  className={`flex w-full rounded-2xl bg-neutral-300 p-3 ${showLess ? "cursor-pointer" : ""}`}
-                >
-                  <div className="flex min-w-0 grow items-center gap-2">
-                    <div className="relative h-10 w-10 shrink-0">
-                      <Image
-                        src={notification.icon}
-                        alt="App icon"
-                        fill
-                        className="absolute inset-0 object-cover"
-                      />
+                      <div className="min-w-0">
+                        <h4 className="mb-0.5 text-sm font-medium text-neutral-950">
+                          {notification.title}
+                        </h4>
+
+                        <p className="truncate text-xs font-normal text-neutral-950">
+                          {notification.message}
+                        </p>
+                      </div>
                     </div>
 
-                    <div className="min-w-0">
-                      <h4 className="mb-0.5 text-sm font-medium text-neutral-950">
-                        {notification.title}
-                      </h4>
-
-                      <p className="truncate text-xs font-normal text-neutral-950">
-                        {notification.message}
-                      </p>
-                    </div>
-                  </div>
-
-                  <span className="inline-flex text-xs font-normal whitespace-nowrap text-neutral-600">
-                    {notification.time}
-                  </span>
-                </motion.div>
-              ))}
-            </div>
-          </motion.div>
-        ) : null}
-      </AnimatePresence>
+                    <span className="inline-flex text-xs font-normal whitespace-nowrap text-neutral-600">
+                      {notification.time}
+                    </span>
+                  </motion.div>
+                ))}
+              </div>
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
+      </div>
     </div>
   );
 };
